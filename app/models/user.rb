7dc1ba5c0b616eef28514ce690c8dcae166de14a
watanabe-tsuguharu <a19871206@gmail.com>
2024-07-24 16:54:18 +0900

    ユーザー登録・ログイン成功

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+validates :nick_name, presence: true 
+validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+validates :password, presence: true, length: { minimum: 6 }, format: 
+            { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "半角英数字を入力してください" }
+validates :password_confirmation, presence: true, confirmation: { case_sensitive: true }
+validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: "全角で入力してください" }
+validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: "全角で入力してください" }
+validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください" }
+validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください" }
+validates :birth_date, presence: true
+
+end 
+
