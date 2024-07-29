9ffb28540909ad5c0d5f8373925ee71f0bfe4c66
watanabe-tsuguharu <a19871206@gmail.com>
2024-07-29 22:03:36 +0900

    ユーザー管理機能２

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 validates :nick_name, presence: true 
-validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-validates :password, presence: true, length: { minimum: 6 }, format: 
-            { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "半角英数字を入力してください" }
-validates :password_confirmation, presence: true, confirmation: { case_sensitive: true }
-validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: "全角で入力してください" }
-validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message: "全角で入力してください" }
+validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "半角英数字を入力してください" }
+validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角で入力してください" }
+validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角で入力してください" }
 validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください" }
 validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください" }
 validates :birth_date, presence: true
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,15 +7,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-
 RSpec.configure do |config|
-  config.before(:each, type: :feature) do
-    page.driver.browser.basic_authorize('admin', 'ADMIN') 
-    # ユーザー名とパスワードを設定
-    config.include FactoryBot::Syntax::Methods
-  end
+  
 end
-
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
