75d35de7879fe0bd7b0d4999a3c27d7061b36e77
watanabe-tsuguharu <a19871206@gmail.com>
2024-08-03 18:24:07 +0900

    商品出品機能-リンク貼り付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   
-  validates :title, presence: true
-  validates :explan, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :region_id, presence: true
-  validates :delivery_date_id, presence: true
-  validates :cost, presence: true, numericality: { greater_than: 0 }
+  validates :title,             presence: true
+  validates :explan,            presence: true
+  validates :category_id,       presence: true
+  validates :condition_id,      presence: true
+  validates :shipping_cost_id,  presence: true
+  validates :region_id,         presence: true
+  validates :delivery_date_id,  presence: true
+  validates :cost,              presence: true, numericality: { greater_than: 0 }
 end
